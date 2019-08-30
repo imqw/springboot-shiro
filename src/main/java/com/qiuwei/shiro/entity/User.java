8d1c294ed4b8d309c1ca4092c177a94eac63cccc
qiuwei <Wcbc0310@126.com>
2019-08-30 11:22:15 +0800

    添加用户对象mapper方法

---
+++ src/main/java/com/qiuwei/shiro/entity/User.java
@@ -0,0 +1,62 @@
+package com.qiuwei.shiro.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 用户
+ * @Author: qiuwei@19pay.com.cn
+ * @Version 1.0.0
+ */
+@Data
+public class User {
+
+    private Long userId;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 盐
+     */
+    private String salt;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 手机号
+     */
+    private String mobile;
+
+    /**
+     * 状态  0：禁用   1：正常
+     */
+    private Integer status;
+
+    /**
+     * 创建者ID
+     */
+    private Long createUserId;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+}
---
+++ src/main/java/com/qiuwei/shiro/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package com.qiuwei.shiro.mapper;
+
+import com.qiuwei.shiro.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author: qiuwei@19pay.com.cn
+ * @Version 1.0.0
+ */
+@Mapper
+public interface UserMapper {
+
+    int delete(Long userId);
+
+    int insert(User user);
+
+
+    User findById(Long userId);
+
+    int updateByParams(User record);
+}
